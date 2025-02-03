49eaedbf736d2216bf763b995c4b0bad66d9b314
boris-valero <157314735+boris-valero@users.noreply.github.com>
2025-02-03 20:52:04 +0100

    Update todolist_base.sql

--- todolist_base.sql
+++ todolist_base.sql
@@ -1,8 +1,9 @@
--- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (i686)
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.5.26-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: todolist_base
 -- ------------------------------------------------------
--- Server version	10.11.6-MariaDB-0+deb12u1
+-- Server version	10.5.26-MariaDB-0+deb11u2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,8 +26,9 @@ DROP TABLE IF EXISTS `todo_list`;
 CREATE TABLE `todo_list` (
   `item_id` int(11) NOT NULL AUTO_INCREMENT,
   `content` varchar(255) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`item_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,11 +37,34 @@ CREATE TABLE `todo_list` (
 
 LOCK TABLES `todo_list` WRITE;
 /*!40000 ALTER TABLE `todo_list` DISABLE KEYS */;
-INSERT INTO `todo_list` VALUES
-(1,'My first important item'),
-(2,'My second important item'),
-(3,'My third important item');
+INSERT INTO `todo_list` VALUES (1,'Tâche 1 de test1',1),(2,'Tâche 2 de test1',1),(3,'Tâche 1 de test2',2),(4,'Tâche 2 de test2',2),(5,'Tâche 1 de test3',3),(6,'Tâche 2 de test3',3),(7,'Tache 1 de l\'utilisateur 3 ',6),(8,'Tache 2 de l\'utilisateur 2',6),(12,'Tache 1 de l\'utilisateur 3',7),(13,'Tache 2 de l\'utilisateur 3',7),(14,'Tache 3 de l\'utilisateur 1',5),(15,'Tache 4 de l\'utilisateur 1',5);
 /*!40000 ALTER TABLE `todo_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (5,'test1@example.com','$2y$10$C8.Y4kFLDOOeNu62Ta3RNu5lcK39W74sJ3sq1NNVImGpx3wiD3B7m'),(6,'test2@example.com','$2y$10$TJwfYDhW2.qAzDKRIyJ18exmJHU.TD6dSSQvppGTmpZ2wAnBNitrG'),(7,'test3@example.com','$2y$10$brf055zf1aBm2rijTg8FT.p8NtZoO8Rgq2GZigI7DBe37U09pf1gy'),(8,'test4@example.com','$2y$10$GJNCA8sJxmIFoXT/EXrqr.bVsiI9.x0p.fLEGL2QXmaprnGZjalrK');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-25 20:52:17
+-- Dump completed on 2025-02-03 20:47:08
